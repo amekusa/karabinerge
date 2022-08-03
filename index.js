d4cd08339b2dd022c335711cac11da6c5cb38a98
amekusa <amekusa@users.noreply.github.com>
2022-08-03 10:20:13 +0900

    Write doc comments

--- index.js
+++ index.js
@@ -51,43 +51,81 @@ class Sanitizer {
   }
 }
 
+/**
+ * A collection of one or more modification rules
+ */
 class RuleSet {
+  /**
+   * @param {string} title - title of this ruleset
+   */
   constructor(title) {
     this.title = title;
     this.rules = [];
   }
+  /**
+   * Creates a rule and adds it to this ruleset
+   * @param {string} desc - rule description
+   * @return {Rule} the created rule
+   */
   add(...args) {
     let rule = new Rule(...args);
     this.rules.push(rule);
     return rule;
   }
+  /**
+   * Returns a plain object representation of this ruleset
+   * @return {object} an object like: `{ title: ... , rules: ... }`
+   */
   toJSON() {
     return {
       title: this.title,
       rules: this.rules.map(item => item.toJSON())
     };
   }
+  /**
+   * Outputs JSON representation of the whole rule set to STDOUT
+   */
   out() {
     console.log(JSON.stringify(this, null, 2));
   }
 }
 
+/**
+ * A complex modification rule
+ */
 class Rule {
+  /**
+   * @param {string} desc - rule description
+   */
   constructor(desc) {
     this.desc = desc;
     this.remaps = [];
     this.conds = [];
   }
+  /**
+   * Defines a `from-to` remap rule
+   * @param {object} map - rule definition like: `{ from: ... , to: ... }`
+   * @return {Rule} this
+   */
   remap(map) {
     if (!map.type) map = { type: 'basic', ...map };
     if (this.conds.length) map = Object.assign(map, { conditions: this.conds });
     this.remaps.push(remapSanitizer.sanitize(map));
     return this;
   }
+  /**
+   * Defines a condition
+   * @param {object} cond - condition definition like: `{ type: 'variable_if', ... }`
+   * @return {Rule} this
+   */
   cond(cond) {
     this.conds.push(cond);
     return this;
   }
+  /**
+   * Returns a plain object representation of this rule
+   * @return {object} an object like: `{ description: ... , manipulators: ... }`
+   */
   toJSON() {
     return {
       description: this.desc,
@@ -96,40 +134,76 @@ class Rule {
   }
 }
 
+/**
+ * User configuration of Karabiner-Elements
+ */
 class Config {
   static new(...args) {
     return new this(...args);
   }
+  /**
+   * @param {string} [file='~/.config/karabiner/karabiner.json'] - config file to read/write
+   */
   constructor(file = null) {
     this.setFile(file || path.join(os.homedir(), '.config', 'karabiner', 'karabiner.json'));
     this.data = null;
   }
+  /**
+   * Sets the config file path
+   * @param {string} file - config file path
+   * @return {Config} this
+   */
   setFile(file) {
     this.file = file;
     return this;
   }
+  /**
+   * Reads data from the config file
+   * @return {Config} this
+   */
   load(file = null) {
     if (!file) file = this.file;
     let content = fs.readFileSync(file, 'utf8');
     this.data = JSON.parse(content);
     return this;
   }
+  /**
+   * Writes the current data on the config file
+   * @return {Config} this
+   */
   save(file = null) {
     if (!file) file = this.file;
     let content = JSON.stringify(this.data, null, 4);
     fs.writeFileSync(file, content, 'utf8');
     return this;
   }
+  /**
+   * Creates a backup of the config file with `.bak` extension in the same directory as the original file.
+   * If an old backup exists, it will be overwritten
+   * @return {Config} this
+   */
   backup() {
     return this.save(this.file + '.bak');
   }
+  /**
+   * Restores data from a backup file
+   * @return {Config} this
+   */
   loadBackup() {
     return this.load(this.file + '.bak');
   }
+  /**
+   * Deletes a backup file
+   * @return {Config} this
+   */
   deleteBackup() {
     fs.unlinkSync(this.file + '.bak');
     return this;
   }
+  /**
+   * The current profile object
+   * @type {object}
+   */
   get currentProfile() {
     if (!this.data) this.load();
     let profs = this.data.profiles;
@@ -139,9 +213,18 @@ class Config {
     }
     throw `no active profile`;
   }
+  /**
+   * Clears all the rules in the current profile
+   * @return {Config} this
+   */
   clearRules() {
     return this.setRules([]);
   }
+  /**
+   * Sets the provided rules to the current profile
+   * @param {object[]} rules - an array of rule definitions
+   * @return {Config} this
+   */
   setRules(rules) {
     dig(this.currentProfile, 'complex_modifications.rules', { set: rules, makePath: true, throw: true });
     return this;
@@ -176,12 +259,24 @@ function arr(x) {
   return Array.isArray(x) ? x : [x];
 }
 
+/**
+ * Returns a `key_code` object
+ * @param {string} code - key code
+ * @param {string|object|string[]} mods - modifiers
+ * @param {object} [opts] - optional properties
+ * @return {object} an object like: `{ key_code: ... }`
+ */
 function key(code, mods = null, opts = null) {
   let r = { key_code: code };
   if (mods) r.modifiers = mods;
   return opts ? Object.assign(r, opts) : r;
 }
 
+/**
+ * Returns a `pointing_button` object
+ * @param {string} btn - button name
+ * @return {object} an object like: `{ pointing_button: ... }`
+ */
 function click(btn) {
   let btns = {
     left: 'button1',
@@ -193,6 +288,13 @@ function click(btn) {
   };
 }
 
+/**
+ * Returns a `set_variable` object
+ * @param {string} name - variable name
+ * @param {string|number} value - value to assign
+ * @param {object} [opts] - optional properties
+ * @return {object} an object like: `{ set_variable: { ... } }`
+ */
 function set_var(name, value, opts = null) {
   let r = {
     set_variable: {
@@ -203,6 +305,12 @@ function set_var(name, value, opts = null) {
   return opts ? Object.assign(r, opts) : r;
 }
 
+/**
+ * Returns a `variable_if` condition object
+ * @param {string} name - variable name
+ * @param {string|number} value - value to check
+ * @return {object} an object like: `{ type: 'variable_if', ... }`
+ */
 function if_var(name, value) {
   return {
     type: 'variable_if',
@@ -211,6 +319,12 @@ function if_var(name, value) {
   };
 }
 
+/**
+ * Returns a `variable_unless` condition object
+ * @param {string} name - variable name
+ * @param {string|number} value - value to check
+ * @return {object} an object like: `{ type: 'variable_unless', ... }`
+ */
 function unless_var(name, value) {
   return {
     type: 'variable_unless',
@@ -219,33 +333,53 @@ function unless_var(name, value) {
   };
 }
 
-function if_app(...ids) {
+/**
+ * Returns a `frontmost_application_if` condition object
+ * @param {...string} id - application id
+ * @return {object} an object like: `{ type: 'frontmost_application_if', ... }`
+ */
+function if_app(...id) {
   return {
     type: 'frontmost_application_if',
-    bundle_identifiers: ids
+    bundle_identifiers: id
   };
 }
 
-function unless_app(...ids) {
+/**
+ * Returns a `frontmost_application_unless` condition object
+ * @param {...string} id - application ID
+ * @return {object} an object like: `{ type: 'frontmost_application_unless', ... }`
+ */
+function unless_app(...id) {
   return {
     type: 'frontmost_application_unless',
-    bundle_identifiers: ids
+    bundle_identifiers: id
   };
 }
 
-function if_lang(...langs) {
+/**
+ * Returns a `input_source_if` condition object
+ * @param {...string} lang - language code
+ * @return {object} an object like: `{ type: 'input_source_if', ... }`
+ */
+function if_lang(...lang) {
   return {
     type: 'input_source_if',
-    input_sources: langs.map(item => {
+    input_sources: lang.map(item => {
       return { language: item }
     })
   };
 }
 
-function unless_lang(...langs) {
+/**
+ * Returns a `input_source_unless` condition object
+ * @param {...string} lang - language code
+ * @return {object} an object like: `{ type: 'input_source_unless', ... }`
+ */
+function unless_lang(...lang) {
   return {
     type: 'input_source_unless',
-    input_sources: langs.map(item => {
+    input_sources: lang.map(item => {
       return { language: item }
     })
   };
