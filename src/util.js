7ee1530c90db41b585a3517409003833c6f975fd
amekusa <hello@amekusa.com>
2024-02-18 09:24:03 +0900

    add util.js

--- src/util.js
+++ src/util.js
@@ -1,12 +1,46 @@
 /**
  * Utility functions
+ * @private
  */
 
-/**
- * Coerce the given value into an array.
- * @param {any} x
- * @returns any[]
- */
 export function arr(x) {
 	return Array.isArray(x) ? x : [x];
+}
+
+export function isEmpty(x) {
+	if (Array.isArray(x)) return x.length == 0;
+	switch (typeof x) {
+	 case 'boolean':
+		return false;
+	 case 'string':
+		return !x;
+	 case 'object':
+		if (x === null) return true;
+		for (let i in x) return false;
+	 case 'undefined':
+		return true;
+	}
+	return false;
+}
+
+export function clean(x, recurse = 8) {
+	if (recurse) {
+		if (Array.isArray(x)) {
+			let r = [];
+			for (let i = 0; i < x.length; i++) {
+				let I = clean(x[i], recurse - 1);
+				if (!isEmpty(I)) r.push(I);
+			}
+			return r;
+		}
+		if (typeof x == 'object') {
+			let r = {};
+			for (let i in x) {
+				let I = clean(x[i], recurse - 1);
+				if (!isEmpty(I)) r[i] = I;
+			}
+			return r;
+		}
+	}
+	return x;
 }
