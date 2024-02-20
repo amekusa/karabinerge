10bc86c271966d77bc6349c32216c406cef5e66d
amekusa <hello@amekusa.com>
2024-02-20 18:17:41 +0900

    [chore] doc comments

--- src/main.js
+++ src/main.js
@@ -40,7 +40,7 @@ export {
 };
 
 /**
- * Returns a `key_code` object
+ * Returns an object with `key_code` property, which can be passed to {@link Rule#remap} as `from` or `to` properties.
  * @param {string} code - key code
  * @param {string|object|string[]} mods - modifiers
  * @param {object} [opts] - optional properties
@@ -53,7 +53,7 @@ export function key(code, mods = null, opts = null) {
 }
 
 /**
- * Returns a `pointing_button` object
+ * Returns an object with `pointing_button` property, which can be passed to {@link Rule#remap} as `from` or `to` properties.
  * @param {string} btn - button name
  * - `button1`
  * - `button2`
@@ -75,7 +75,7 @@ export function click(btn) {
 }
 
 /**
- * Returns a `set_variable` object
+ * Returns an object with `set_variable` property, which can be passed to {@link Rule#remap} as `to` property.
  * @param {string} name - variable name
  * @param {string|number} value - value to assign
  * @param {object} [opts] - optional properties
@@ -92,7 +92,7 @@ export function set_var(name, value, opts = null) {
 }
 
 /**
- * Returns a `variable_if` condition object
+ * Returns an object with `type: 'variable_if'` property, which can be passed to {@link Rule#cond} as a condition.
  * @param {string} name - variable name
  * @param {string|number} value - value to check
  * @return {object} an object like: `{ type: 'variable_if', ... }`
@@ -106,7 +106,7 @@ export function if_var(name, value) {
 }
 
 /**
- * Returns a `variable_unless` condition object
+ * Returns an object with `type: 'variable_unless'` property, which can be passed to {@link Rule#cond} as a condition.
  * @param {string} name - variable name
  * @param {string|number} value - value to check
  * @return {object} an object like: `{ type: 'variable_unless', ... }`
@@ -120,7 +120,7 @@ export function unless_var(name, value) {
 }
 
 /**
- * Returns a `frontmost_application_if` condition object
+ * Returns an object with `type: 'frontmost_application_if'` property, which can be passed to {@link Rule#cond} as a condition.
  * @param {...string} id - application id
  * @return {object} an object like: `{ type: 'frontmost_application_if', ... }`
  */
@@ -132,8 +132,8 @@ export function if_app(...id) {
 }
 
 /**
- * Returns a `frontmost_application_unless` condition object
- * @param {...string} id - application ID
+ * Returns an object with `type: 'frontmost_application_unless'` property, which can be passed to {@link Rule#cond} as a condition.
+ * @param {...string} id - application id
  * @return {object} an object like: `{ type: 'frontmost_application_unless', ... }`
  */
 export function unless_app(...id) {
@@ -144,7 +144,7 @@ export function unless_app(...id) {
 }
 
 /**
- * Returns a `input_source_if` condition object
+ * Returns an object with `type: 'input_source_if'` property, which can be passed to {@link Rule#cond} as a condition.
  * @param {...string} lang - language code
  * @return {object} an object like: `{ type: 'input_source_if', ... }`
  */
@@ -158,7 +158,7 @@ export function if_lang(...lang) {
 }
 
 /**
- * Returns a `input_source_unless` condition object
+ * Returns an object with `type: 'input_source_unless'` property, which can be passed to {@link Rule#cond} as a condition.
  * @param {...string} lang - language code
  * @return {object} an object like: `{ type: 'input_source_unless', ... }`
  */
