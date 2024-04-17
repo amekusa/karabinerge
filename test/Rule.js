34de6a1af07505f8f13e57ea483dc93029d67919
amekusa <hello@amekusa.com>
2024-04-18 00:31:44 +0900

    test: update

--- test/Rule.js
+++ test/Rule.js
@@ -68,40 +68,146 @@ testInstance(Rule, {
 	},
 	'remap': {
 		test(rule) {
-			rule.remap({from: 'a', to: 'b'});
+			rule.remap({
+				from: {key_code: 'a'},
+				to:  [{key_code: 'b'}]
+			});
+			dseq(rule.remaps, [{
+				type: 'basic',
+				from: {key_code: 'a'},
+				to:  [{key_code: 'b'}]
+			}]);
+		}
+	},
+	'remap (multiple)': {
+		test(rule) {
+			seq(rule.remap({
+				from: {key_code: 'a'},
+				to:  [{key_code: 'b'}]
+			}), rule); // must return itself
+
+			rule.remap({
+				from: {key_code: 'b'},
+				to:  [{key_code: 'c'}]
+			})
+			.remap({
+				from: {key_code: 'c'},
+				to:  [{key_code: 'd'}]
+			});
 			dseq(rule.remaps, [
 				{
 					type: 'basic',
-					from: 'a',
-					to: ['b'],
-				}
+					from: {key_code: 'a'},
+					to:  [{key_code: 'b'}]
+				},
+				{
+					type: 'basic',
+					from: {key_code: 'b'},
+					to:  [{key_code: 'c'}]
+				},
+				{
+					type: 'basic',
+					from: {key_code: 'c'},
+					to:  [{key_code: 'd'}]
+				},
 			]);
 		}
 	},
-	'multi-remap': {
+	'remap + string expr.': {
 		test(rule) {
-			let r = rule.remap({from: 'a', to: 'b'});
-			seq(r, rule);
-
-			r.remap({from: 'c', to: 'd'})
-			 .remap({from: 'e', to: 'f'});
-			dseq(rule.remaps, [
-				{
-					type: 'basic',
-					from: 'a',
-					to: ['b'],
-				},
-				{
-					type: 'basic',
-					from: 'c',
-					to: ['d'],
-				},
-				{
-					type: 'basic',
-					from: 'e',
-					to: ['f'],
-				},
-			]);
+			rule.remap({from: 'a', to: 'b'});
+			dseq(rule.remaps, [{
+				type: 'basic',
+				from: {key_code: 'a'},
+				to:  [{key_code: 'b'}]
+			}]);
 		}
-	}
+	},
+	'remap + string expr. (multiple-to)': {
+		test(rule) {
+			rule.remap({from: 'a', to: ['b', 'c']});
+			dseq(rule.remaps, [{
+				type: 'basic',
+				from: {key_code: 'a'},
+				to:  [
+					{key_code: 'b'},
+					{key_code: 'c'}
+				]
+			}]);
+		}
+	},
+	'remap + string expr. (modifiers)': {
+		test(rule) {
+			rule.remap({
+				from: 'shift + a',
+				to:   'control + b'
+			});
+			dseq(rule.remaps, [{
+				type: 'basic',
+				from: {
+					key_code: 'a',
+					modifiers: {
+						mandatory: ['shift']
+					}
+				},
+				to: [{
+					key_code: 'b',
+					modifiers: ['control']
+				}]
+			}]);
+		}
+	},
+	'remap + string expr. (optional modifiers)': {
+		test(rule) {
+			rule.remap({
+				from: 'shift + (control) + a',
+				to:   'control + b'
+			});
+			dseq(rule.remaps, [{
+				type: 'basic',
+				from: {
+					key_code: 'a',
+					modifiers: {
+						mandatory: ['shift'],
+						optional:  ['control']
+					}
+				},
+				to: [{
+					key_code: 'b',
+					modifiers: ['control']
+				}]
+			}]);
+		}
+	},
+	'remap + string expr. (multiple-to with modifiers)': {
+		test(rule) {
+			rule.remap({
+				from: 'shift + (control) + a',
+				to: [
+					'control + b',
+					'shift + command + c',
+				]
+			});
+			dseq(rule.remaps, [{
+				type: 'basic',
+				from: {
+					key_code: 'a',
+					modifiers: {
+						mandatory: ['shift'],
+						optional:  ['control']
+					}
+				},
+				to: [
+					{
+						key_code: 'b',
+						modifiers: ['control']
+					},
+					{
+						key_code: 'c',
+						modifiers: ['shift', 'command']
+					}
+				]
+			}]);
+		}
+	},
 });
