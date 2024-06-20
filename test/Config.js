86fa5f61d0f9cf78ea9ce020147447f368e0045f
amekusa <hello@amekusa.com>
2024-06-20 12:44:08 +0900

    tests: update Config

--- test/Config.js
+++ test/Config.js
@@ -19,69 +19,72 @@ import {Config} from '../dist/import/bundle.mjs';
 import fs from 'node:fs';
 const secrets = JSON.parse(fs.readFileSync('./test/secrets.json'));
 
-testInstance(Config, {
-	'no args': {
-		args: [],
-		props: {
-			file: secrets.home + '/.config/karabiner/karabiner.json'
-		}
-	},
-	'file': {
-		args: ['foo/bar'],
-		props: {
-			file: 'foo/bar'
-		}
-	},
-	'file with tilde': {
-		args: ['~/foo/bar'],
-		props: {
-			file: secrets.home + '/foo/bar'
-		}
-	},
-	'test file': {
-		args: ['./test/config.json'],
-		props: {
-			file: './test/config.json'
+describe(`class: Config`, () => {
+
+	testMethod(Config, 'setIO', {
+		'no args': {
+			returnsSelf: true,
+			test(x) {
+				eq(x.io.file, secrets.home + '/.config/karabiner/karabiner.json');
+			}
 		},
-		test(x) {
-			x.load();
-			eq(x.data.profiles.length, 3);
-			eq(x.data.profiles[0].selected, true);
-			seq(x.currentProfile, x.data.profiles[0]);
-			eq(x.currentProfile.name, 'Default profile');
-		}
-	}
+		'file': {
+			args: ['foo/bar'],
+			returnsSelf: true,
+			test(x) {
+				eq(x.io.file, 'foo/bar');
+			}
+		},
+		'file with tilde': {
+			args: ['~/foo/bar'],
+			test(x) {
+				eq(x.io.file, secrets.home + '/foo/bar');
+			}
+		},
+		'test file': {
+			args: ['./test/config.json'],
+			test(x) {
+				eq(x.io.file, './test/config.json');
+				x.load();
+				eq(x.data.profiles.length, 3);
+				eq(x.data.profiles[0].selected, true);
+				seq(x.currentProfile, x.data.profiles[0]);
+				eq(x.currentProfile.name, 'Default profile');
+			}
+		},
+	});
+
+	testMethod(() => new Config().setIO('./test/config.json'), 'selectProfile', {
+		'index': {
+			args: [1],
+			test(x) {
+				eq(x.data.profiles[0].selected, false);
+				eq(x.data.profiles[1].selected, true);
+				eq(x.data.profiles[2].selected, false);
+				seq(x.currentProfile, x.data.profiles[1]);
+				eq(x.currentProfile.name, 'Profile Alice');
+			}
+		},
+		'name': {
+			args: ['Profile Alice'],
+			test(x) {
+				eq(x.data.profiles[0].selected, false);
+				eq(x.data.profiles[1].selected, true);
+				eq(x.data.profiles[2].selected, false);
+				seq(x.currentProfile, x.data.profiles[1]);
+				eq(x.currentProfile.name, 'Profile Alice');
+			}
+		},
+		'regex': {
+			args: [/Bob/],
+			test(x) {
+				eq(x.data.profiles[0].selected, false);
+				eq(x.data.profiles[1].selected, false);
+				eq(x.data.profiles[2].selected, true);
+				seq(x.currentProfile, x.data.profiles[2]);
+				eq(x.currentProfile.name, 'Profile Bob');
+			}
+		},
+	});
 });
 
-testMethod(() => new Config('./test/config.json'), 'selectProfile', {
-	'index': {
-		args: [1],
-		test(x) {
-			eq(x.data.profiles[0].selected, false);
-			eq(x.data.profiles[1].selected, true);
-			eq(x.data.profiles[2].selected, false);
-			seq(x.currentProfile, x.data.profiles[1]);
-			eq(x.currentProfile.name, 'Profile Alice');
-		}
-	},
-	'name': {
-		args: ['Profile Alice'],
-		test(x) {
-			eq(x.data.profiles[0].selected, false);
-			eq(x.data.profiles[1].selected, true);
-			eq(x.data.profiles[2].selected, false);
-			seq(x.currentProfile, x.data.profiles[1]);
-			eq(x.currentProfile.name, 'Profile Alice');
-		}
-	},
-	'regex': {
-		args: [/Bob/],
-		test(x) {
-			eq(x.data.profiles[0].selected, false);
-			eq(x.data.profiles[1].selected, false);
-			eq(x.data.profiles[2].selected, true);
-			seq(x.currentProfile, x.data.profiles[2]);
-			eq(x.currentProfile.name, 'Profile Bob');
-		}
-	}
-});
