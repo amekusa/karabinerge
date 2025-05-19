6afa0ce5dc89f50b4a92a44de6bc64561e499b9c
Satoshi Soma <noreply@amekusa.com>
2025-05-20 06:57:20 +0900

    [dev] test: describe more

--- test/Rule.js
+++ test/Rule.js
@@ -10,204 +10,207 @@ const {testFn, testMethod, testInstance} = test;
 
 import {Rule} from '../dist/import/bundle.mjs';
 
-testMethod(Rule, 'fromJSON', {
-	'object': {
-		args: [{
-			description: 'Test Rule',
-			manipulators: [
-				{
-					type: 'basic',
-					from: {key_code: 'a'},
-					to:  [{key_code: 'b'}]
-				}
-			]
-		}],
-		returnType: Rule,
-		test(r) {
-			eq(r.desc, 'Test Rule');
-			deq(r.remaps, [
-				{
-					type: 'basic',
-					from: {key_code: 'a'},
-					to:  [{key_code: 'b'}]
-				}
-			]);
-		}
-	},
-	'JSON string': {
-		args: [`{
-			"description": "Test Rule",
-			"manipulators": [
-				{
-					"type": "basic",
-					"from": {"key_code": "a"},
-					"to":  [{"key_code": "b"}]
-				}
-			]
-		}`],
-		returnType: Rule,
-		test(r) {
-			eq(r.desc, 'Test Rule');
-			deq(r.remaps, [
-				{
-					type: 'basic',
-					from: {key_code: 'a'},
-					to:  [{key_code: 'b'}]
-				}
-			]);
-		}
-	}
-}, {static: true, strict: true});
+describe(`class: Rule`, () => {
 
-testInstance(Rule, {
-	'desc': {
-		args: ['This is a test rule.'],
-		props: {
-			desc: 'This is a test rule.',
+	testMethod(Rule, 'fromJSON', {
+		'object': {
+			args: [{
+				description: 'Test Rule',
+				manipulators: [
+					{
+						type: 'basic',
+						from: {key_code: 'a'},
+						to:  [{key_code: 'b'}]
+					}
+				]
+			}],
+			returnType: Rule,
+			test(r) {
+				eq(r.desc, 'Test Rule');
+				deq(r.remaps, [
+					{
+						type: 'basic',
+						from: {key_code: 'a'},
+						to:  [{key_code: 'b'}]
+					}
+				]);
+			}
+		},
+		'JSON string': {
+			args: [`{
+				"description": "Test Rule",
+				"manipulators": [
+					{
+						"type": "basic",
+						"from": {"key_code": "a"},
+						"to":  [{"key_code": "b"}]
+					}
+				]
+			}`],
+			returnType: Rule,
+			test(r) {
+				eq(r.desc, 'Test Rule');
+				deq(r.remaps, [
+					{
+						type: 'basic',
+						from: {key_code: 'a'},
+						to:  [{key_code: 'b'}]
+					}
+				]);
+			}
 		}
-	},
-	'remap': {
-		test(rule) {
-			rule.remap({
-				from: {key_code: 'a'},
-				to:  [{key_code: 'b'}]
-			});
-			dseq(rule.remaps, [{
-				type: 'basic',
-				from: {key_code: 'a'},
-				to:  [{key_code: 'b'}]
-			}]);
-		}
-	},
-	'remap (multiple)': {
-		test(rule) {
-			seq(rule.remap({
-				from: {key_code: 'a'},
-				to:  [{key_code: 'b'}]
-			}), rule); // must return itself
+	}, {static: true, strict: true});
 
-			rule.remap({
-				from: {key_code: 'b'},
-				to:  [{key_code: 'c'}]
-			})
-			.remap({
-				from: {key_code: 'c'},
-				to:  [{key_code: 'd'}]
-			});
-			dseq(rule.remaps, [
-				{
+	testInstance(Rule, {
+		'desc': {
+			args: ['This is a test rule.'],
+			props: {
+				desc: 'This is a test rule.',
+			}
+		},
+		'remap': {
+			test(rule) {
+				rule.remap({
+					from: {key_code: 'a'},
+					to:  [{key_code: 'b'}]
+				});
+				dseq(rule.remaps, [{
 					type: 'basic',
 					from: {key_code: 'a'},
 					to:  [{key_code: 'b'}]
-				},
-				{
-					type: 'basic',
+				}]);
+			}
+		},
+		'remap (multiple)': {
+			test(rule) {
+				seq(rule.remap({
+					from: {key_code: 'a'},
+					to:  [{key_code: 'b'}]
+				}), rule); // must return itself
+
+				rule.remap({
 					from: {key_code: 'b'},
 					to:  [{key_code: 'c'}]
-				},
-				{
-					type: 'basic',
+				})
+				.remap({
 					from: {key_code: 'c'},
 					to:  [{key_code: 'd'}]
-				},
-			]);
-		}
-	},
-	'remap + string expr.': {
-		test(rule) {
-			rule.remap({from: 'a', to: 'b'});
-			dseq(rule.remaps, [{
-				type: 'basic',
-				from: {key_code: 'a'},
-				to:  [{key_code: 'b'}]
-			}]);
-		}
-	},
-	'remap + string expr. (multiple-to)': {
-		test(rule) {
-			rule.remap({from: 'a', to: ['b', 'c']});
-			dseq(rule.remaps, [{
-				type: 'basic',
-				from: {key_code: 'a'},
-				to:  [
-					{key_code: 'b'},
-					{key_code: 'c'}
-				]
-			}]);
-		}
-	},
-	'remap + string expr. (modifiers)': {
-		test(rule) {
-			rule.remap({
-				from: 'shift + a',
-				to:   'control + b'
-			});
-			dseq(rule.remaps, [{
-				type: 'basic',
-				from: {
-					key_code: 'a',
-					modifiers: {
-						mandatory: ['shift']
-					}
-				},
-				to: [{
-					key_code: 'b',
-					modifiers: ['control']
-				}]
-			}]);
-		}
-	},
-	'remap + string expr. (optional modifiers)': {
-		test(rule) {
-			rule.remap({
-				from: 'shift + (control) + a',
-				to:   'control + b'
-			});
-			dseq(rule.remaps, [{
-				type: 'basic',
-				from: {
-					key_code: 'a',
-					modifiers: {
-						mandatory: ['shift'],
-						optional:  ['control']
-					}
-				},
-				to: [{
-					key_code: 'b',
-					modifiers: ['control']
-				}]
-			}]);
-		}
-	},
-	'remap + string expr. (multiple-to with modifiers)': {
-		test(rule) {
-			rule.remap({
-				from: 'shift + (control) + a',
-				to: [
-					'control + b',
-					'shift + command + c',
-				]
-			});
-			dseq(rule.remaps, [{
-				type: 'basic',
-				from: {
-					key_code: 'a',
-					modifiers: {
-						mandatory: ['shift'],
-						optional:  ['control']
-					}
-				},
-				to: [
+				});
+				dseq(rule.remaps, [
 					{
-						key_code: 'b',
-						modifiers: ['control']
+						type: 'basic',
+						from: {key_code: 'a'},
+						to:  [{key_code: 'b'}]
 					},
 					{
-						key_code: 'c',
-						modifiers: ['shift', 'command']
-					}
-				]
-			}]);
-		}
-	},
+						type: 'basic',
+						from: {key_code: 'b'},
+						to:  [{key_code: 'c'}]
+					},
+					{
+						type: 'basic',
+						from: {key_code: 'c'},
+						to:  [{key_code: 'd'}]
+					},
+				]);
+			}
+		},
+		'remap + string expr.': {
+			test(rule) {
+				rule.remap({from: 'a', to: 'b'});
+				dseq(rule.remaps, [{
+					type: 'basic',
+					from: {key_code: 'a'},
+					to:  [{key_code: 'b'}]
+				}]);
+			}
+		},
+		'remap + string expr. (multiple-to)': {
+			test(rule) {
+				rule.remap({from: 'a', to: ['b', 'c']});
+				dseq(rule.remaps, [{
+					type: 'basic',
+					from: {key_code: 'a'},
+					to:  [
+						{key_code: 'b'},
+						{key_code: 'c'}
+					]
+				}]);
+			}
+		},
+		'remap + string expr. (modifiers)': {
+			test(rule) {
+				rule.remap({
+					from: 'shift + a',
+					to:   'control + b'
+				});
+				dseq(rule.remaps, [{
+					type: 'basic',
+					from: {
+						key_code: 'a',
+						modifiers: {
+							mandatory: ['shift']
+						}
+					},
+					to: [{
+						key_code: 'b',
+						modifiers: ['control']
+					}]
+				}]);
+			}
+		},
+		'remap + string expr. (optional modifiers)': {
+			test(rule) {
+				rule.remap({
+					from: 'shift + (control) + a',
+					to:   'control + b'
+				});
+				dseq(rule.remaps, [{
+					type: 'basic',
+					from: {
+						key_code: 'a',
+						modifiers: {
+							mandatory: ['shift'],
+							optional:  ['control']
+						}
+					},
+					to: [{
+						key_code: 'b',
+						modifiers: ['control']
+					}]
+				}]);
+			}
+		},
+		'remap + string expr. (multiple-to with modifiers)': {
+			test(rule) {
+				rule.remap({
+					from: 'shift + (control) + a',
+					to: [
+						'control + b',
+						'shift + command + c',
+					]
+				});
+				dseq(rule.remaps, [{
+					type: 'basic',
+					from: {
+						key_code: 'a',
+						modifiers: {
+							mandatory: ['shift'],
+							optional:  ['control']
+						}
+					},
+					to: [
+						{
+							key_code: 'b',
+							modifiers: ['control']
+						},
+						{
+							key_code: 'c',
+							modifiers: ['shift', 'command']
+						}
+					]
+				}]);
+			}
+		},
+	});
 });
--- test/main.js
+++ test/main.js
@@ -5,168 +5,171 @@ import {
 	key
 } from '../dist/import/bundle.mjs';
 
-testFn(key, {
-	'simple key': {
-		args: ['a'],
-		return: {key_code: 'a'}
-	},
-	'number key': {
-		args: [0],
-		return: {key_code: '0'}
-	},
-	'single modifier': {
-		args: ['a', 'control'],
-		return: {
-			key_code: 'a',
-			modifiers: ['control']
-		}
-	},
-	'multiple modifiers': {
-		args: ['a', ['control', 'shift']],
-		return: {
-			key_code: 'a',
-			modifiers: ['control', 'shift']
-		}
-	},
-	'optional modifier': {
-		args: ['a', {mandatory: 'control', optional: 'shift'}],
-		return: {
-			key_code: 'a',
-			modifiers: {
-				mandatory: ['control'],
-				optional: ['shift']
+describe(`functions:`, () => {
+
+	testFn(key, {
+		'simple key': {
+			args: ['a'],
+			return: {key_code: 'a'}
+		},
+		'number key': {
+			args: [0],
+			return: {key_code: '0'}
+		},
+		'single modifier': {
+			args: ['a', 'control'],
+			return: {
+				key_code: 'a',
+				modifiers: ['control']
 			}
-		}
-	},
-	'multiple mandatory & optional modifiers': {
-		args: ['a', {mandatory: ['control', 'command'], optional: ['shift', 'option']}],
-		return: {
-			key_code: 'a',
-			modifiers: {
-				mandatory: ['control', 'command'],
-				optional: ['shift', 'option']
+		},
+		'multiple modifiers': {
+			args: ['a', ['control', 'shift']],
+			return: {
+				key_code: 'a',
+				modifiers: ['control', 'shift']
 			}
-		}
-	},
-	'options': {
-		args: [
-			'a', {mandatory: 'control', optional: 'shift'},
-			{
+		},
+		'optional modifier': {
+			args: ['a', {mandatory: 'control', optional: 'shift'}],
+			return: {
+				key_code: 'a',
+				modifiers: {
+					mandatory: ['control'],
+					optional: ['shift']
+				}
+			}
+		},
+		'multiple mandatory & optional modifiers': {
+			args: ['a', {mandatory: ['control', 'command'], optional: ['shift', 'option']}],
+			return: {
+				key_code: 'a',
+				modifiers: {
+					mandatory: ['control', 'command'],
+					optional: ['shift', 'option']
+				}
+			}
+		},
+		'options': {
+			args: [
+				'a', {mandatory: 'control', optional: 'shift'},
+				{
+					str: 'abc',
+					num: 0,
+					bool: false,
+					arr: [0, 1, 2],
+					obj: {x: 0, y: 1, z: 2}
+				}
+			],
+			return: {
+				key_code: 'a',
+				modifiers: {
+					mandatory: ['control'],
+					optional: ['shift']
+				},
 				str: 'abc',
 				num: 0,
 				bool: false,
 				arr: [0, 1, 2],
 				obj: {x: 0, y: 1, z: 2}
 			}
-		],
-		return: {
-			key_code: 'a',
-			modifiers: {
-				mandatory: ['control'],
-				optional: ['shift']
-			},
-			str: 'abc',
-			num: 0,
-			bool: false,
-			arr: [0, 1, 2],
-			obj: {x: 0, y: 1, z: 2}
-		}
-	},
+		},
 
-	'multiple keys': {
-		args: [['a', 'b', 'c']],
-		return: [
-			{key_code: 'a'},
-			{key_code: 'b'},
-			{key_code: 'c'}
-		]
-	},
-	'multiple keys with a common modifier': {
-		args: [['a', 'b', 'c'], 'control'],
-		return: [
-			{key_code: 'a', modifiers: ['control']},
-			{key_code: 'b', modifiers: ['control']},
-			{key_code: 'c', modifiers: ['control']},
-		]
-	},
-	'multiple keys with common modifiers and options': {
-		args: [['a', 'b', 'c'], ['control', 'shift'], {x: 1, y: 2, z: 3}],
-		return: [
-			{key_code: 'a', modifiers: ['control', 'shift'], x: 1, y: 2, z: 3},
-			{key_code: 'b', modifiers: ['control', 'shift'], x: 1, y: 2, z: 3},
-			{key_code: 'c', modifiers: ['control', 'shift'], x: 1, y: 2, z: 3},
-		]
-	},
-	'multiple keys with modifiers for each': {
-		args: [[
-			['a', 'control'],
-			['b', 'shift'],
-			['c', ['control', 'shift']],
-		]],
-		return: [
-			{key_code: 'a', modifiers: ['control']},
-			{key_code: 'b', modifiers: ['shift']},
-			{key_code: 'c', modifiers: ['control', 'shift']},
-		]
-	},
+		'multiple keys': {
+			args: [['a', 'b', 'c']],
+			return: [
+				{key_code: 'a'},
+				{key_code: 'b'},
+				{key_code: 'c'}
+			]
+		},
+		'multiple keys with a common modifier': {
+			args: [['a', 'b', 'c'], 'control'],
+			return: [
+				{key_code: 'a', modifiers: ['control']},
+				{key_code: 'b', modifiers: ['control']},
+				{key_code: 'c', modifiers: ['control']},
+			]
+		},
+		'multiple keys with common modifiers and options': {
+			args: [['a', 'b', 'c'], ['control', 'shift'], {x: 1, y: 2, z: 3}],
+			return: [
+				{key_code: 'a', modifiers: ['control', 'shift'], x: 1, y: 2, z: 3},
+				{key_code: 'b', modifiers: ['control', 'shift'], x: 1, y: 2, z: 3},
+				{key_code: 'c', modifiers: ['control', 'shift'], x: 1, y: 2, z: 3},
+			]
+		},
+		'multiple keys with modifiers for each': {
+			args: [[
+				['a', 'control'],
+				['b', 'shift'],
+				['c', ['control', 'shift']],
+			]],
+			return: [
+				{key_code: 'a', modifiers: ['control']},
+				{key_code: 'b', modifiers: ['shift']},
+				{key_code: 'c', modifiers: ['control', 'shift']},
+			]
+		},
 
-	'string expression :: single modifier': {
-		args: ['control + a'],
-		return: {
-			key_code: 'a',
-			modifiers: ['control']
-		}
-	},
-	'string expression :: multiple modifiers': {
-		args: ['control + shift + a'],
-		return: {
-			key_code: 'a',
-			modifiers: ['control', 'shift']
-		}
-	},
-	'string expression :: optional modifier': {
-		args: ['control + (shift) + a'],
-		return: {
-			key_code: 'a',
-			modifiers: {
-				mandatory: ['control'],
-				optional: ['shift']
+		'string expression :: single modifier': {
+			args: ['control + a'],
+			return: {
+				key_code: 'a',
+				modifiers: ['control']
 			}
-		}
-	},
-	'string expression :: multiple mandatory & optional modifiers': {
-		args: ['control + command + (shift) + (option) + a'],
-		return: {
-			key_code: 'a',
-			modifiers: {
-				mandatory: ['control', 'command'],
-				optional: ['shift', 'option']
+		},
+		'string expression :: multiple modifiers': {
+			args: ['control + shift + a'],
+			return: {
+				key_code: 'a',
+				modifiers: ['control', 'shift']
 			}
-		}
-	},
-	'string expression :: multiple keys': {
-		args: [[
-			'control + a',
-			'control + shift + a',
-			'(control) + a',
-			'control + (shift) + a',
-			'control + command + (shift) + (option) + a',
-		]],
-		return: [
-			{key_code: 'a', modifiers: ['control']},
-			{key_code: 'a', modifiers: ['control', 'shift']},
-			{key_code: 'a', modifiers: {
-				optional: ['control']
-			}},
-			{key_code: 'a', modifiers: {
-				mandatory: ['control'],
-				optional: ['shift']
-			}},
-			{key_code: 'a', modifiers: {
-				mandatory: ['control', 'command'],
-				optional: ['shift', 'option']
-			}},
-		]
-	},
+		},
+		'string expression :: optional modifier': {
+			args: ['control + (shift) + a'],
+			return: {
+				key_code: 'a',
+				modifiers: {
+					mandatory: ['control'],
+					optional: ['shift']
+				}
+			}
+		},
+		'string expression :: multiple mandatory & optional modifiers': {
+			args: ['control + command + (shift) + (option) + a'],
+			return: {
+				key_code: 'a',
+				modifiers: {
+					mandatory: ['control', 'command'],
+					optional: ['shift', 'option']
+				}
+			}
+		},
+		'string expression :: multiple keys': {
+			args: [[
+				'control + a',
+				'control + shift + a',
+				'(control) + a',
+				'control + (shift) + a',
+				'control + command + (shift) + (option) + a',
+			]],
+			return: [
+				{key_code: 'a', modifiers: ['control']},
+				{key_code: 'a', modifiers: ['control', 'shift']},
+				{key_code: 'a', modifiers: {
+					optional: ['control']
+				}},
+				{key_code: 'a', modifiers: {
+					mandatory: ['control'],
+					optional: ['shift']
+				}},
+				{key_code: 'a', modifiers: {
+					mandatory: ['control', 'command'],
+					optional: ['shift', 'option']
+				}},
+			]
+		},
 
-}, 'deepEqual');
+	}, 'deepEqual');
+});
